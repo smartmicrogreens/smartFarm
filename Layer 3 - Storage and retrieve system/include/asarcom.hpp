ffc6b6916f87c90bdeef468f5010df2dcfa08913
Emanuel Gomez Arn <egomezarn@gmail.com>
2020-04-05 00:35:32 +0900

    Adding new class for implementing WiFi. Class is empty

---
+++ Layer 3 - Storage and retrieve system/include/asarcom.hpp
@@ -0,0 +1,14 @@
+#ifndef ASARCOM_HPP
+#define ASARCOM_HPP
+
+#include <Arduino.h>
+
+class ASARCOM {
+private:
+
+public:
+    ASARCOM();
+
+};
+
+#endif // ASARCOM_HPP
---
+++ Layer 3 - Storage and retrieve system/src/asarcom.cpp
@@ -0,0 +1,5 @@
+#include <asarcom.hpp>
+
+ASARCOM::ASARCOM() {
+    
+}
