eaa2008ba1be45f7b21eb1ff529e05b813a30cad
Emanuel Gomez Arn <egomezarn@gmail.com>
2020-04-05 22:07:55 +0900

    Read and write from ESP8266 completed.

--- Layer 3 - Storage and retrieve system/ASARServer/include/server.hpp
+++ Layer 3 - Storage and retrieve system/ASARServer/include/server.hpp
@@ -2,18 +2,24 @@
 #define SERVER_H
 
 #include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+
+#define STACK_PROTECTOR 512
 
 class ASARServer {
 private:
-    WiFiServer* server;
-    WiFiClient* client;
-
     const char* ssid = "testing";
     const char* password = "1234567890";
+    const int MAX_SRV_CLIENTS = 2;
+    //const size_t STACK_PROTECTOR = 512;
 
+    WiFiServer* server;
+    WiFiClient* clients;
+    //SoftwareSerial* logger;
 public:
     ASARServer();
-    String read();
+    void displayConnectedUsers();
+    void read();
 };
 
 #endif
--- Layer 3 - Storage and retrieve system/ASARServer/src/server.cpp
+++ Layer 3 - Storage and retrieve system/ASARServer/src/server.cpp
@@ -1,9 +1,11 @@
 #include "server.hpp"
 
 ASARServer::ASARServer() {
+
     Serial.begin(115200);
     delay(1000);
-    server = new WiFiServer(80);
+    server = new WiFiServer(23);
+    clients = new WiFiClient;
     WiFi.mode(WIFI_STA);
     WiFi.begin("testing", "1234567890");
     Serial.print("Connecting");
@@ -12,31 +14,48 @@ ASARServer::ASARServer() {
         Serial.print(".");
     }
     Serial.println(" ");
+    server->setNoDelay(true);
     Serial.print("Connected to WiFi. IP:");
     Serial.println(WiFi.localIP());
     
     server->begin();
     //client = new WiFiClient();
 }
-String ASARServer::read() {
-    client = new WiFiClient( server->available() );
- 
-  if (client) {
-    //uint8_t buffer[160];
-    while (client->connected()) {
- 
-        while (client->available() > 0) {
-            char c = client->read();
-            Serial.write(c);
-        //char c = client->read(buffer, 160);
+void ASARServer::displayConnectedUsers() {
+    size_t maxToTcp = 0;
+    if (server->hasClient())
+        if (!clients->available()) { // equivalent to !serverClients[i].connected()
+            *clients = server->available();
+            Serial.print("Client connected .-");
         }
- 
-        delay(10);
-        client->stop();
-        Serial.println("Client disconnected");
+
+    while (clients->available() && Serial.availableForWrite() > 0) {
+        // working char by char is not very efficient
+        Serial.write(clients->read());
+
+        maxToTcp = 0;
+        if (clients) {
+            size_t afw = clients->availableForWrite();
+            if (afw) {
+                if (!maxToTcp) maxToTcp = afw;
+                else maxToTcp = std::min(maxToTcp, afw);
+            } 
+        }
     }
- 
-  }
-  delete client;
-  return "OK";
+    
+    // Checks UART data
+    size_t len = std::min((size_t)Serial.available(), maxToTcp);
+    len = std::min(len, (size_t)STACK_PROTECTOR);
+    if (len) {
+        uint8_t sbuf[len];
+        size_t serial_got = Serial.readBytes(sbuf, len);
+        // push UART data to all connected telnet clients
+        // if client.availableForWrite() was 0 (congested)
+        // and increased since then,
+        // ensure write space is sufficient:
+        if (clients->availableForWrite() >= serial_got) {
+            size_t tcp_sent = clients->write(sbuf, serial_got);
+            if (tcp_sent != len)  Serial.printf("len mismatch: available:%zd serial-read:%zd tcp-write:%zd\n", len, serial_got, tcp_sent);
+        }
+    }
 }
