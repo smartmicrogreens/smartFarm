fe60ea78ec2e76cb10d0b5e484d2f66667d0e7cb
Robin Hornak <robin.hornak@rakuten.com>
2020-05-30 15:36:52 +0900

    updates

--- Layer 2 - Edge computing (RasPi)/tastehood/tastehood_server/cli/main.py
+++ Layer 2 - Edge computing (RasPi)/tastehood/tastehood_server/cli/main.py
@@ -13,9 +13,11 @@ def main():
 
 @main.command()
 @click.help_option()
-def start_server():
+@click.option('--host', type=str, default='127.0.0.1', help='Host')
+@click.option('--port', type=int, default=5000, help='Port')
+def start_server(port, host):
     """Start tastehood server."""
-    uvicorn.run("tastehood_server.server:app", host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run("tastehood_server.server:app", host=host, port=port, log_level="info")
 
 
 
